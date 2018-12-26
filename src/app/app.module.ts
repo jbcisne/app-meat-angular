17f85528edca67cde4120202c6fbaffc1e8e14e1
Juliano Buzanello <jbcisne@gmail.com>
2018-12-26 21:43:08 -0200

    Implementando validações do form, refatorando para modulos e iniciando animações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { RouterModule, PreloadAllModules } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { HeaderComponent } from './header/header.component';
 
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 
-import { OrderService } from './order/order.service';
-import { OrderComponent } from './order/order.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
-
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
 
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
@@ -39,7 +28,6 @@ import { RatingComponent } from './shared/rating/rating.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
@@ -47,22 +35,17 @@ import { RatingComponent } from './shared/rating/rating.component';
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
     OrderSummaryComponent,
-    RatingComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(ROUTES)
+    BrowserAnimationsModule,
+    SharedModule.forRoot(),
+    //preloadingStrategy carregas os modulos lazy loading em background apos o carregamento do modulo root
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,43 @@
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+import { InputComponent } from "./input/input.component";
+import { RadioComponent } from "./radio/radio.component";
+import { RatingComponent } from "./rating/rating.component";
+
+import { ShoppingCartService } from "../restaurant-detail/shopping-cart/shopping-cart.service";
+import { OrderService } from "../order/order.service";
+import { RestaurantsService } from "../restaurants/restaurants.service";
+import { SnackbarComponent } from "./messages/snackbar/snackbar.component";
+
+@NgModule({
+    declarations: [
+     InputComponent,
+     RadioComponent,
+     RatingComponent,
+     SnackbarComponent,
+    ],
+    imports:[
+        FormsModule,
+        ReactiveFormsModule,
+        CommonModule,
+    ],
+    exports:[
+        FormsModule,
+        ReactiveFormsModule,
+        CommonModule,
+        SnackbarComponent,
+        InputComponent,
+        RadioComponent,
+        RatingComponent,
+    ]
+})
+export class SharedModule{
+  static forRoot(): ModuleWithProviders {
+      return {
+          ngModule: SharedModule,
+          providers:[ShoppingCartService, OrderService, RestaurantsService]
+      }
+  }
+}
